d669052c337863c3abafc88b9b90a475a20a04d4
MLTPL <jefftefflessmess@atomicmail.io>
2026-08-13 15:05:33 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "liumial-community-chat",
+  "version": "1.0.0",
+  "description": "Static chat app hosted on Express",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=14.0.0"
+  }
+}
